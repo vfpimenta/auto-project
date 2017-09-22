1ac6e74ba8c47592b5d30b78463afbdb2980e69f
vfpimenta <vfpimenta.93@gmail.com>
2017-09-21 22:17:54 -0300

    Completed main analysis.

--- phase 2/visits.R
+++ phase 2/visits.R
@@ -1,4 +1,6 @@
 library(base)
+library(ggmap)
+library(ggplot2)
 
 values.num <- function(list) {
   as.numeric(paste(unlist(list)))
@@ -28,6 +30,10 @@ inc <- function(el) {
   }
 }
 
+get.day <- function(date){
+  as.integer(substr(date,9,10))
+}
+
 get.month <- function(date) {
   as.integer(substr(date,6,7))
 }
@@ -49,6 +55,14 @@ for (name in unique(as.character(data$place_name))) {
   series[[name]]$visits <- integer(length(seq(start.date,end.date,by='days')))
 }
 
+wd <- list("domingo"=0,"segunda"=0,"terça"=0,
+  "quarta"=0,"quinta"=0,"sexta"=0,"sábado"=0)
+
+md <- list()
+for (i in 1:31) {
+  md[[i]] = 0
+}
+
 for (i in 1:length(data$dt)) {
   name <- as.character(data$place_name[i])
   date <- as.Date(data$dt[i])
@@ -57,29 +71,40 @@ for (i in 1:length(data$dt)) {
 
   series[[name]]$lat <- c(series[[name]]$lat, as.double(data$lat[i]))
   series[[name]]$lng <- c(series[[name]]$lng, as.double(data$lng[i]))
+
+  wd[[weekdays(date)]] <- inc(wd[[weekdays(date)]])
+  md[[get.day(date)]] <- inc(md[[get.day(date)]])
 }
+
+ts.means <- c()
+lat.means <- c()
+lng.means <- c()
 
 for (name in names(series)) {
   series[[name]]$ts <- ts(series[[name]]$visits, frequency=365,
     start=c(get.year(start.date), as.numeric(format(start.date, "%j"))))
+
+  ts.means <- c(ts.means, mean(series[[name]]$ts))
+  lat.means <- c(lat.means, mean(series[[name]]$lat))
+  lng.means <- c(lng.means, mean(series[[name]]$lng))
 }
 
-plot(1, xlim=c(-8.2,-7.85), ylim=c(-35,-34.82))
-output <- list()
-for (name in names(series)) {
-  points(mean(series[[name]]$lat), mean(series[[name]]$lng))
-  output[['lat']] <- c(output[['lat']], mean(series[[name]]$lat))
-  output[['lng']] <- c(output[['lng']], mean(series[[name]]$lng))
-}
-write.csv(output, file='location-output.csv', row.names=FALSE)
+barplot(values.num(wd), names.arg=names(wd))
+barplot(values.num(md), names.arg=as.character(1:31))
+
+# plot(1, xlim=c(-8.2,-7.85), ylim=c(-35,-34.82))
+# for (name in names(series)) {
+#   points(mean(series[[name]]$lng), mean(series[[name]]$lat),
+#     cex=mean(series[[name]]$ts), pch=21, bg='black')
+# }
+
+pe <- c(left = -35, top = -7.85, right = -34.82, bottom = -8.2)
+map <- get_stamenmap(pe, zoom = 12, maptype = "toner-background")
+p <- ggmap(map)
+d <- data.frame(lat=lat.means, lng=lng.means)
+p <- p + geom_point(data=d, aes(x=lng, y=lat), size=ts.means, color='red')
+ggplot_build(p)
 
 # for (key in names(series)) {
 #   plot(series[[key]]$ts, ylab=key)
 # }
-
-# avg <- list()
-# for (key in names(series)) {
-#   avg[[key]] <- mean(series[[key]]$ts)
-# }
-# avg <- avg[avg > 1.9]
-# plot(values.num(avg), ylab='average')
